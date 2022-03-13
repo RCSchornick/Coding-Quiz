df7ff7e57bb82a1ee51479f961627da7673b41a5
RCSchornick <94027300+RCSchornick@users.noreply.github.com>
2022-03-13 00:48:42 -0600

    adding css and questions

---
+++ assets/script.js
@@ -0,0 +1,51 @@
+
+
+
+
+var codeQuestions = [{
+    title: "1. What symbol combination creates a CSS comment?",
+    choices: {
+       a: '/*',
+       b: '//',
+       c: '*|',
+       d: '?$',
+    },
+    answer: '/*',
+    },
+    {title: "2. What git command should you use to create a new file?",
+    choices: {
+        a: 'pull',
+        b: 'merge',
+        c: 'touch',
+        d: 'explore',
+    },
+    answer: 'touch',
+    },
+    {title: "3. What is the term for creating a skeleton of an HTML document?",
+    choices: {
+        a: 'bricking',
+        b: 'scaffolding',
+        c: 'building',
+        d: 'roofing',
+    },
+    answer: 'touch',
+    },
+    {title: "4. What is a set of HTML code that builds part of the overall page's UI commonly referred to?",
+    choices: {
+        a: 'component',
+        b: 'piece',
+        c: 'element',
+        d: 'unit',
+    },
+    answer: 'component',
+    },
+    {question: "5. What is the git command is used to save your work to your repository?",
+    choices: {
+        a: 'save',
+        b: 'merge',
+        c: 'init',
+        d: 'pull/push',
+    },
+    answer : 'pull/push'    
+    },
+]
